d8053938e3d125a0b6bca32fcceeab5dbd94f2e8
Walter Rodrigues <metal.walter@gmail.com>
2023-05-28 15:44:42 -0300

    Iniciando a movimentação das peças

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,10 +14,23 @@ namespace xadrez_console
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPposicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPposicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+
             }
+
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
