ac7f7cfe935e0d2c375c03e9ddbde136c3c00b93
Walter Rodrigues <metal.walter@gmail.com>
2023-05-30 16:41:06 -0300

    Implementados roque grande e roque pequeno

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,11 +1,14 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace xadrez_console.tabuleiro.xadrez
 {
-    internal class Rei : Peca
+    class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        private PartidaDeXadrez partida;
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -18,6 +21,13 @@ namespace xadrez_console.tabuleiro.xadrez
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
         }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
+        }
+
         public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
@@ -79,6 +89,38 @@ namespace xadrez_console.tabuleiro.xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // Implementando a jogada especial roque
+            if (qteMovimentos == 0 && !partida.xeque)
+            {// Jogada especial roque pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna +1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna +2);
+                    if (tab.peca(p1)==null && tab.peca(p2)==null)
+                    {
+                        mat[posicao.linha, pos.coluna + 2] = true;
+                    }
+                }
+
+                // Jogada especial roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna -4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, pos.coluna - 2] = true;
+                    }
+                }
+
+            }
+
+
+
             return mat;
         }
 
